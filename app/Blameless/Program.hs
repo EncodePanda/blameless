35290964d59af9b42f7aef6d194dd56f814df8af
EncodePanda <paul.szulc@gmail.com>
2025-03-11 00:00:06 +0100

    Program should throwError when run in a folder that is not a git repository

--- app/Blameless/Program.hs
+++ app/Blameless/Program.hs
@@ -1,13 +1,19 @@
-module Blameless.Program (program) where
+module Blameless.Program (program, ProgramErr) where
 
 import Blameless.Effects.Console
 import Blameless.Effects.Git
 import Control.Monad (unless)
 import Effectful
+import Effectful.Error.Dynamic
 
-program :: (Git :> es, Console :> es) => Eff es ()
+data ProgramErr = DirNotGitRepository
+  deriving stock Show
+
+program :: (Git :> es, Console :> es, Error ProgramErr :> es) => Eff es ()
 program = do
   -- Check if running in a Git repository
   repo <- isGitRepo
-  unless repo $ logError "Not in a Git repository"
+  unless repo $ do
+    logError "Not in a Git repository"
+    throwError DirNotGitRepository
   pure ()
--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,21 @@ module Main (main) where
 import Blameless.Effects.Console
 import Blameless.Effects.FileSystem
 import Blameless.Effects.Git
-import Blameless.Program (program)
+import Blameless.Program (ProgramErr, program)
+import Data.Either.Combinators (whenLeft)
 import Data.Function ((&))
 import Effectful
+import Effectful.Error.Dynamic (runError)
+import System.Exit (exitFailure)
 
 main :: IO ()
-main =
-  program
-    & runGit
-    & runFileSystem
-    & runConsole
-    & runEff
+main = do
+  resErr <-
+    program
+      & runGit
+      & runFileSystem
+      & runConsole
+      & runError @ProgramErr
+      & runEff
+
+  whenLeft resErr $ const exitFailure
